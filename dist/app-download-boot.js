9255275ce8a6b8e5b8f02bfe6222a8a845d6a960
capricorncd <1355952333@qq.com>
2017-08-30 23:59:09 +0800

    新增ios系统版本>=9时启动app处理逻辑

--- dist/app-download-boot.js
+++ dist/app-download-boot.js
@@ -2,168 +2,215 @@
  * Created by Capricorncd on 2017/6/28 0028.
  * https://github.com/capricorncd
  */
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && (define.amd || define.cmd) ? define(factory) :
-            global.appDownloadBoot = factory();
-}(this, function () {
-    'use strict';
-    var appDownloadBoot = (function () {
-        function appDownloadBoot(params) {
-            // 微信
-            this.isWeixin = !!(/MicroMessenger/ig).test(navigator.userAgent);
-            // Android Device
-            this.isAndroid = !!navigator.userAgent.match(/android/ig);
-            // IOS Device
-            this.isIos = !!navigator.userAgent.match(/(iphone|ipod|ios|ipad)/ig);
-            this.opts = this.concatOptions(params);
-            // 创建meta标签，只对ios6及以上safari浏览器有效
-            if (this.isIos) {
-                this.createIosMeta();
+var appDownloadBoot = (function () {
+    function appDownloadBoot(params) {
+        // 微信
+        this.isWeixin = !!(/MicroMessenger/ig).test(navigator.userAgent);
+        // Android Device
+        this.isAndroid = !!navigator.userAgent.match(/android/ig);
+        // IOS Device
+        this.isIos = !!navigator.userAgent.match(/(iphone|ipod|ios|ipad)/ig);
+        this.iosVersion = this.getIosVersion();
+        this.opts = this.concatOptions(params);
+        // 创建meta标签，只对ios6及以上safari浏览器有效
+        this.createIosMeta();
+    }
+    // 合并默认配置参数与用户设置的参数
+    appDownloadBoot.prototype.concatOptions = function (opts) {
+        opts = opts instanceof Object ? opts : {};
+        var obj = {};
+        for (var key in opts) {
+            obj[key] = opts[key];
+        }
+        // 默认配置
+        var _DEFAULT = {
+            android: {
+                file: null,
+                schema: null,
+                yyb: null
+            },
+            ios: {
+                appid: null,
+                schema: null,
+                appstore: null
+            },
+            site: null,
+            url: null
+        };
+        for (var key1 in _DEFAULT) {
+            if (!obj[key1]) {
+                this.opts.callback && this.opts.callback({
+                    code: 1,
+                    msg: key1 + " \u672A\u914D\u7F6E"
+                });
+                obj[key1] = _DEFAULT[key1];
+            }
+            if (_DEFAULT[key1] instanceof Object) {
+                for (var k in _DEFAULT[key1]) {
+                    var val = _DEFAULT[key1][k];
+                    try {
+                        if (!obj[key1][k]) {
+                            this.opts.callback && this.opts.callback({
+                                code: 2,
+                                msg: key1 + "\u5C5E\u6027" + k + " \u672A\u914D\u7F6E"
+                            });
+                            obj[key1][k] = val;
+                        }
+                    }
+                    catch (e) {
+                    }
+                }
             }
         }
-        // 合并默认配置参数与用户设置的参数
-        appDownloadBoot.prototype.concatOptions = function (opts) {
-            opts = opts instanceof Object ? opts : {};
-            var obj = {};
-            for (var key in opts) {
-                obj[key] = opts[key];
-            }
-            // 默认配置
-            var _DEFAULT = {
-                android: {
-                    file: null,
-                    schema: null,
-                    yyb: null
-                },
-                ios: {
-                    appid: null,
-                    schema: null,
-                    appstore: null
-                },
-                site: null,
-                url: null
-            };
-            for (var key1 in _DEFAULT) {
-                if (!obj[key1]) {
-                    this.opts.callback && this.opts.callback({
-                        code: 1,
-                        msg: key1 + " \u672A\u914D\u7F6E"
-                    });
-                    obj[key1] = _DEFAULT[key1];
-                }
-                if (_DEFAULT[key1] instanceof Object) {
-                    for (var k in _DEFAULT[key1]) {
-                        var val = _DEFAULT[key1][k];
-                        try {
-                            if (!obj[key1][k]) {
-                                this.opts.callback && this.opts.callback({
-                                    code: 2,
-                                    msg: key1 + "\u5C5E\u6027" + k + " \u672A\u914D\u7F6E"
-                                });
-                                obj[key1][k] = val;
+        return obj;
+    };
+    // 点击下载
+    appDownloadBoot.prototype.download = function () {
+        var _this = this;
+        // 非移动设备，直接跳转至设置的官网
+        if (!this.isAndroid && !this.isIos) {
+            this.opts.callback && this.opts.callback({
+                code: 7,
+                msg: '非移动设备'
+            });
+            window.location.href = this.opts.url;
+            return;
+        }
+        var android = this.opts.android || {};
+        var ios = this.opts.ios || {};
+        // Android
+        if (this.isAndroid) {
+            // 尝试启动app
+            this.tryStarting(android.schema, function (res) {
+                if (res.code > 0) {
+                    // 微信浏览器 =》应用宝
+                    // 非微信浏览器：访问apk文件，直接下载
+                    if (_this.isWeixin) {
+                        window.location.href = android.yyb;
+                    }
+                    else {
+                        _this.createIframe(android.file, function (res2) {
+                            // 下载文件失败，实际获取不到结果?
+                            if (res2.code > 0) {
+                                window.location.href = this.opts.site;
                             }
-                        }
-                        catch (e) {
-                        }
+                        });
                     }
                 }
-            }
-            return obj;
-        };
-        // 点击下载
-        appDownloadBoot.prototype.download = function () {
-            var _this = this;
-            // 非移动设备，直接跳转至设置的官网
-            if (!this.isAndroid && !this.isIos) {
-                this.opts.callback && this.opts.callback({
-                    code: 7,
-                    msg: '非移动设备'
-                });
-                window.location.href = this.opts.url;
+            });
+            this.opts.callback && this.opts.callback({
+                code: 5,
+                msg: 'andriod设备'
+            });
+        }
+        else if (this.isIos) {
+            // ios version >= 9
+            if (this.iosVersion >= 9) {
+                window.location.href = this.opts.url ? ios.schema + "?url=" + this.opts.url : ios.schema;
+                var timer_1 = setTimeout(function () {
+                    // 阻塞页面跳转，让用户有时间点击启动App按钮
+                    window.alert('即将进入appStore！');
+                    window.location.href = ios.appstore;
+                    clearTimeout(timer_1);
+                }, 500);
                 return;
             }
-            var android = this.opts.android || {};
-            var ios = this.opts.ios || {};
-            // Android
-            if (this.isAndroid) {
-                // 尝试启动app
-                this.tryStarting(android.schema, function (status) {
-                    if (!status) {
-                        // 微信浏览器 =》应用宝
-                        // 非微信浏览器：访问apk文件，直接下载
-                        window.location.href = _this.isWeixin ? android.yyb : android.file;
-                    }
-                });
-                this.opts.callback && this.opts.callback({
-                    code: 5,
-                    msg: 'andriod设备'
-                });
-            }
-            else if (this.isIos) {
-                // 尝试启动app
-                this.tryStarting(ios.schema, function (status) {
-                    if (!status) {
-                        // 进入appStore
-                        window.location.href = ios.appstore;
-                    }
-                });
-                this.opts.callback && this.opts.callback({
-                    code: 6,
-                    msg: 'ios设备'
-                });
-            }
-        };
-        // 尝试启动app
-        appDownloadBoot.prototype.tryStarting = function (schemaUrl, callback) {
-            var _this = this;
-            // 创建iframe
-            // 通过iframe.src尝试启动app
-            if (schemaUrl) {
-                var iframe_1 = document.createElement('iframe');
-                iframe_1.style.cssText = 'display:none;width=0;height=0';
-                iframe_1.src = schemaUrl + "?url=" + this.opts.url;
-                iframe_1.onload = function () {
-                    callback && callback(true);
+            // ios version < 9
+            // 尝试启动app
+            this.tryStarting(ios.schema, function (res) {
+                if (res.code > 0) {
+                    // 进入appStore
+                    window.location.href = ios.appstore;
+                }
+            });
+            this.opts.callback && this.opts.callback({
+                code: 6,
+                msg: 'ios设备'
+            });
+        }
+    };
+    // 尝试启动app
+    appDownloadBoot.prototype.tryStarting = function (schemaUrl, callback) {
+        var _this = this;
+        // 创建iframe
+        // 通过iframe.src尝试启动app
+        if (schemaUrl) {
+            this.createIframe(schemaUrl, function (res) {
+                callback && callback(res);
+                if (res.code === 0) {
                     _this.opts.callback && _this.opts.callback({
                         code: 3,
                         msg: '启动App成功'
                     });
-                };
-                iframe_1.onerror = function () {
-                    callback && callback(false);
+                }
+                else {
                     _this.opts.callback && _this.opts.callback({
                         code: 4,
-                        msg: '启动App失败'
+                        msg: '启动App失败 ' + schemaUrl
                     });
-                };
-                document.body.appendChild(iframe_1);
-                // 移除iframe
-                var timer_1 = setTimeout(function () {
-                    document.body.removeChild(iframe_1);
-                    clearTimeout(timer_1);
-                }, 1000);
-                this.opts.callback && this.opts.callback({
-                    code: 8,
-                    msg: '尝试启动app: ' + iframe_1.src
-                });
-            }
-            else {
-                this.opts.callback && this.opts.callback({
-                    code: 9,
-                    msg: '未配置schema协议'
-                });
-            }
-        };
-        // 创建meta标签
-        appDownloadBoot.prototype.createIosMeta = function () {
+                }
+            });
+            this.opts.callback && this.opts.callback({
+                code: 8,
+                msg: '尝试启动app: ' + schemaUrl
+            });
+        }
+        else {
+            this.opts.callback && this.opts.callback({
+                code: 9,
+                msg: '未配置schema协议'
+            });
+        }
+    };
+    // 获取ios系统版本号
+    appDownloadBoot.prototype.getIosVersion = function () {
+        return /OS (\d+)/ig.test(navigator.userAgent) ? RegExp.$1 : 0;
+    };
+    // 创建meta标签
+    appDownloadBoot.prototype.createIosMeta = function () {
+        if (this.isIos) {
             var appid = this.opts.ios.appid;
             var meta = document.createElement('meta');
             meta.setAttribute('name', 'apple-itunes-app');
             meta.setAttribute('content', "app-id='" + appid + "', affiliate-data=myAffiliateData, app-argument='" + appid + "'");
             document.querySelector('head').appendChild(meta);
+        }
+    };
+    // 创建iframe
+    appDownloadBoot.prototype.createIframe = function (url, callback) {
+        var iframe = document.createElement('iframe');
+        iframe.style.cssText = 'display:none;width=0;height=0';
+        iframe.src = url;
+        document.body.appendChild(iframe);
+        // 超时处理
+        var overtime = setTimeout(function () {
+            callback && callback({
+                code: 2,
+                msg: 'overtime'
+            });
+            clearTimeout(overtime);
+        }, 3000);
+        // onload
+        iframe.onload = function () {
+            clearTimeout(overtime);
+            callback && callback({
+                code: 0,
+                msg: 'success'
+            });
         };
-        return appDownloadBoot;
-    }());
+        // onerror
+        iframe.onerror = function () {
+            clearTimeout(overtime);
+            callback && callback({
+                code: 1,
+                msg: 'error'
+            });
+        };
+        // 移除iframe
+        var timer = setTimeout(function () {
+            document.body.removeChild(iframe);
+            clearTimeout(timer);
+        }, 1500);
+    };
     return appDownloadBoot;
-}));
+}());
--- src/app-download-boot.ts
+++ src/app-download-boot.ts
@@ -2,177 +2,228 @@
  * Created by Capricorncd on 2017/6/28 0028.
  * https://github.com/capricorncd
  */
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && (define.amd || define.cmd) ? define(factory) :
-            global.appDownloadBoot = factory();
-}(this, function () {
-    'use strict'
 
-    class appDownloadBoot {
-        // 微信
-        isWeixin: boolean = !!(/MicroMessenger/ig).test(navigator.userAgent)
-        // Android Device
-        isAndroid: boolean = !!navigator.userAgent.match(/android/ig)
-        // IOS Device
-        isIos: boolean = !!navigator.userAgent.match(/(iphone|ipod|ios|ipad)/ig)
-        opts: any
+class appDownloadBoot {
+    // 微信
+    isWeixin = !!(/MicroMessenger/ig).test(navigator.userAgent)
+    // Android Device
+    isAndroid = !!navigator.userAgent.match(/android/ig)
+    // IOS Device
+    isIos = !!navigator.userAgent.match(/(iphone|ipod|ios|ipad)/ig)
+    iosVersion = this.getIosVersion()
+    opts: any
 
-        constructor(params) {
-            this.opts = this.concatOptions(params)
-            // 创建meta标签，只对ios6及以上safari浏览器有效
-            if (this.isIos) {
-                this.createIosMeta()
-            }
+    constructor(params) {
+        this.opts = this.concatOptions(params)
+        // 创建meta标签，只对ios6及以上safari浏览器有效
+        this.createIosMeta()
+    }
+
+    // 合并默认配置参数与用户设置的参数
+    concatOptions(opts) {
+        opts = opts instanceof Object ? opts : {}
+        let obj = {}
+        for (let key in opts) {
+            obj[key] = opts[key]
+        }
+        // 默认配置
+        let _DEFAULT: any = {
+            android: {
+                file: null,
+                schema: null,
+                yyb: null
+            },
+            ios: {
+                appid: null,
+                schema: null,
+                appstore: null
+            },
+            site: null,
+            url: null
         }
 
-        // 合并默认配置参数与用户设置的参数
-        concatOptions(opts) {
-            opts = opts instanceof Object ? opts : {}
-            let obj = {}
-            for (let key in opts) {
-                obj[key] = opts[key]
-            }
-            // 默认配置
-            let _DEFAULT: any = {
-                android: {
-                    file: null,
-                    schema: null,
-                    yyb: null
-                },
-                ios: {
-                    appid: null,
-                    schema: null,
-                    appstore: null
-                },
-                site: null,
-                url: null
+        for (let key1 in _DEFAULT) {
+            if (!obj[key1]) {
+                this.opts.callback && this.opts.callback({
+                    code: 1,
+                    msg: `${key1} 未配置`
+                })
+                obj[key1] = _DEFAULT[key1]
             }
 
-            for (let key1 in _DEFAULT) {
-                if (!obj[key1]) {
-                    this.opts.callback && this.opts.callback({
-                        code: 1,
-                        msg: `${key1} 未配置`
-                    })
-                    obj[key1] = _DEFAULT[key1]
-                }
-
-                if (_DEFAULT[key1] instanceof Object) {
-                    for (let k in _DEFAULT[key1]) {
-                        let val = _DEFAULT[key1][k]
-                        try {
-                            if (!obj[key1][k]) {
-                                this.opts.callback && this.opts.callback({
-                                    code: 2,
-                                    msg: `${key1}属性${k} 未配置`
-                                })
-                                obj[key1][k] = val
-                            }
-                        } catch (e) {
+            if (_DEFAULT[key1] instanceof Object) {
+                for (let k in _DEFAULT[key1]) {
+                    let val = _DEFAULT[key1][k]
+                    try {
+                        if (!obj[key1][k]) {
+                            this.opts.callback && this.opts.callback({
+                                code: 2,
+                                msg: `${key1}属性${k} 未配置`
+                            })
+                            obj[key1][k] = val
                         }
+                    } catch (e) {
                     }
                 }
             }
-            return obj
+        }
+        return obj
+    }
+
+    // 点击下载
+    download() {
+        // 非移动设备，直接跳转至设置的官网
+        if (!this.isAndroid && !this.isIos) {
+            this.opts.callback && this.opts.callback({
+                code: 7,
+                msg: '非移动设备'
+            });
+            window.location.href = this.opts.url
+            return
         }
 
-        // 点击下载
-        download() {
-            // 非移动设备，直接跳转至设置的官网
-            if (!this.isAndroid && !this.isIos) {
-                this.opts.callback && this.opts.callback({
-                    code: 7,
-                    msg: '非移动设备'
-                });
-                window.location.href = this.opts.url
+        let android = this.opts.android || {}
+        let ios = this.opts.ios || {}
+        // Android
+        if (this.isAndroid) {
+            // 尝试启动app
+            this.tryStarting(android.schema, (res) => {
+                if (res.code > 0) {
+                    // 微信浏览器 =》应用宝
+                    // 非微信浏览器：访问apk文件，直接下载
+                    if (this.isWeixin) {
+                        window.location.href = android.yyb
+                    } else {
+                        this.createIframe(android.file, function (res2) {
+                            // 下载文件失败，实际获取不到结果?
+                            if (res2.code > 0) {
+                                window.location.href = this.opts.site
+                            }
+                        });
+                    }
+                }
+            })
+            this.opts.callback && this.opts.callback({
+                code: 5,
+                msg: 'andriod设备'
+            })
+        }
+        // Ios
+        else if (this.isIos) {
+            // ios version >= 9
+            if (this.iosVersion >= 9) {
+                window.location.href = this.opts.url ? `${ios.schema}?url=${this.opts.url}` : ios.schema
+                let timer = setTimeout(() => {
+                    // 阻塞页面跳转，让用户有时间点击启动App按钮
+                    window.alert('即将进入appStore！')
+                    window.location.href = ios.appstore
+                    clearTimeout(timer)
+                }, 500)
                 return
             }
-
-            let android = this.opts.android || {}
-            let ios = this.opts.ios || {}
-            // Android
-            if (this.isAndroid) {
-                // 尝试启动app
-                this.tryStarting(android.schema, (status) => {
-                    if (!status) {
-                        // 微信浏览器 =》应用宝
-                        // 非微信浏览器：访问apk文件，直接下载
-                        window.location.href = this.isWeixin ? android.yyb : android.file
-                    }
-                })
-                this.opts.callback && this.opts.callback({
-                    code: 5,
-                    msg: 'andriod设备'
-                })
-            }
-            // Ios
-            else if (this.isIos) {
-                // 尝试启动app
-                this.tryStarting(ios.schema, (status) => {
-                    if (!status) {
-                        // 进入appStore
-                        window.location.href = ios.appstore
-                    }
-                })
-                this.opts.callback && this.opts.callback({
-                    code: 6,
-                    msg: 'ios设备'
-                })
-            }
+            // ios version < 9
+            // 尝试启动app
+            this.tryStarting(ios.schema, (res) => {
+                if (res.code > 0) {
+                    // 进入appStore
+                    window.location.href = ios.appstore;
+                }
+            })
+            this.opts.callback && this.opts.callback({
+                code: 6,
+                msg: 'ios设备'
+            })
         }
+    }
 
-        // 尝试启动app
-        tryStarting(schemaUrl, callback) {
-            // 创建iframe
-            // 通过iframe.src尝试启动app
-            if (schemaUrl) {
-                let iframe = document.createElement('iframe')
-                iframe.style.cssText = 'display:none;width=0;height=0'
-                iframe.src = `${schemaUrl}?url=${this.opts.url}`
-                iframe.onload = () => {
-                    callback && callback(true)
+    // 尝试启动app
+    tryStarting(schemaUrl, callback) {
+        // 创建iframe
+        // 通过iframe.src尝试启动app
+        if (schemaUrl) {
+
+            this.createIframe(schemaUrl, (res) => {
+                callback && callback(res)
+                if (res.code === 0) {
                     this.opts.callback && this.opts.callback({
                         code: 3,
                         msg: '启动App成功'
                     })
-                }
-                iframe.onerror = () => {
-                    callback && callback(false)
+                } else {
                     this.opts.callback && this.opts.callback({
                         code: 4,
-                        msg: '启动App失败'
+                        msg: '启动App失败 ' + schemaUrl
                     })
                 }
-                document.body.appendChild(iframe)
-                // 移除iframe
-                let timer = setTimeout(() => {
-                    document.body.removeChild(iframe)
-                    clearTimeout(timer)
-                }, 1000)
-                this.opts.callback && this.opts.callback({
-                    code: 8,
-                    msg: '尝试启动app: ' + iframe.src
-                })
-            } else {
-                this.opts.callback && this.opts.callback({
-                    code: 9,
-                    msg: '未配置schema协议'
-                })
-            }
-        }
+            })
 
-        // 创建meta标签
-        createIosMeta() {
+            this.opts.callback && this.opts.callback({
+                code: 8,
+                msg: '尝试启动app: ' + schemaUrl
+            })
+        } else {
+            this.opts.callback && this.opts.callback({
+                code: 9,
+                msg: '未配置schema协议'
+            })
+        }
+    }
+
+    // 获取ios系统版本号
+    getIosVersion() {
+        return /OS (\d+)/ig.test(navigator.userAgent) ? RegExp.$1 : 0
+    }
+
+    // 创建meta标签
+    createIosMeta() {
+        if (this.isIos) {
             let appid = this.opts.ios.appid
             let meta = document.createElement('meta')
             meta.setAttribute('name', 'apple-itunes-app')
             meta.setAttribute('content', `app-id='${appid}', affiliate-data=myAffiliateData, app-argument='${appid}'`)
             document.querySelector('head').appendChild(meta)
         }
-
     }
 
-    return appDownloadBoot
+    // 创建iframe
+    createIframe(url, callback) {
+        var iframe = document.createElement('iframe')
+        iframe.style.cssText = 'display:none;width=0;height=0'
+        iframe.src = url
+        document.body.appendChild(iframe)
 
-}))
+        // 超时处理
+        let overtime = setTimeout(() => {
+            callback && callback({
+                code: 2,
+                msg: 'overtime'
+            })
+            clearTimeout(overtime)
+        }, 3000)
+
+        // onload
+        iframe.onload = () => {
+            clearTimeout(overtime)
+            callback && callback({
+                code: 0,
+                msg: 'success'
+            })
+        }
+
+        // onerror
+        iframe.onerror = () => {
+            clearTimeout(overtime)
+            callback && callback({
+                code: 1,
+                msg: 'error'
+            })
+        }
+        // 移除iframe
+        let timer = setTimeout(() => {
+            document.body.removeChild(iframe)
+            clearTimeout(timer)
+        }, 1500)
+    }
+
+}
+
